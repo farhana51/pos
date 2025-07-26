7110274ed8c4a9c9c86db96ee74d8e56bccd7302
Galib Qurishi <gqurishi@gmail.com>
2025-07-26 23:36:15 +0000

    remove the  LocationIQ  fullty I not use any more .. now use only the Ma

--- src/app/(app)/delivery/new/page.tsx
+++ src/app/(app)/delivery/new/page.tsx
@@ -240,18 +240,4 @@ function NewDeliveryOrderPage() {
     )
 }
 
-function useDebounce<T>(value: T, delay?: number): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value)
-
-  useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
-
-    return () => {
-      clearTimeout(timer)
-    }
-  }, [value, delay])
-
-  return debouncedValue
-}
-
 export default withAuth(NewDeliveryOrderPage, ['Admin' as UserRole, 'Advanced' as UserRole, 'Basic' as UserRole]);
